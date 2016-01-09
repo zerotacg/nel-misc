e241f3cf4ef2e3949ace56dab892dd8e90f33a16
Tobias Peters <tobias.peters@kreativeffekt.at>
2016-01-10 00:54:41 +0100

    - implemented serialization of CRGBA

--- src/nel/misc/c_rgba.js
+++ src/nel/misc/c_rgba.js
@@ -1,7 +1,20 @@
 /**
  * @class nlmisc.CRGBA
+ * @implements nlio.ISerializable
  */
 export default class CRGBA {
+    static readFrom( stream ) {
+        var instance = new CRGBA(0, 0, 0);
+
+        instance.readFrom(stream);
+
+        return instance;
+    }
+
+    static writeTo( stream, instance ) {
+        instance.writeTo(stream);
+    }
+
     /**
      * @constructor
      * @param {uint8} r
@@ -9,10 +22,24 @@ export default class CRGBA {
      * @param {uint8} b
      * @param {uint8} [a=255]
      */
-    constructor(r, g, b, a = 255) {
+    constructor( r, g, b, a = 255 ) {
         this.r = r;
         this.g = g;
         this.b = b;
         this.a = a;
+    }
+
+    readFrom( stream ) {
+        this.r = stream.readUint8();
+        this.g = stream.readUint8();
+        this.b = stream.readUint8();
+        this.a = stream.readUint8();
+    }
+
+    writeTo( stream ) {
+        stream.writeUint8(this.r);
+        stream.writeUint8(this.g);
+        stream.writeUint8(this.b);
+        stream.writeUint8(this.a);
     }
 }
